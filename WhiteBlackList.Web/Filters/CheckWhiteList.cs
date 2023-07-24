4968c674ac96f8a3b2bad3e2f2ee9de68209cc36
Taylan ALTUN <taylanaltun@gmail.com>
2023-07-24 10:33:41 +0300

    Add: Notes,  WhiteBlackList.Web project, IPList and  IPSafeMiddleWare

---
+++ WhiteBlackList.Web/Filters/CheckWhiteList.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WhiteBlackList.Web.Filters
+{
+    // ActionFilterAttribute filterlar syesinde controllerdaki herhangi bir methoda istek gelmeden önce/sonra  methoda girdikten/çıktıktan sonra gelen istekler yakalanabiliyor.
+    public class CheckWhiteList:ActionFilterAttribute
+    {
+    }
+}
---
+++ WhiteBlackList.Web/MiddleWares/IPSafeMiddleWare.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+
+namespace WhiteBlackList.Web.MiddleWares
+{
+
+    // Program.cs içinde de aşağıdaki kod bloğu ile eklendi.
+    // app.UseMiddleware<IPSafeMiddleWare>();
+
+    public class IPSafeMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        private readonly IPList _ipList;
+
+
+        // IPList doldurmak için IOptions interface kulllanılıyor. Program.cs içinde de aşağıdaki kod bloğu kullanıldı.
+        // builder.Services.Configure<IPList>(builder.Configuration.GetSection("IPList"));
+        public IPSafeMiddleWare(RequestDelegate next, IOptions<IPList> ipList)
+        {
+            _next = next;
+            _ipList = ipList.Value;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // API den gelen isteğin adresini al.
+            var reqIpAddress=httpContext.Connection.RemoteIpAddress;
+
+            var isWhiteList = _ipList.WhiteList.Where(x => IPAddress.Parse(x).Equals(reqIpAddress)).Any();
+
+            if(!isWhiteList)
+            {
+                httpContext.Response.StatusCode=(int) HttpStatusCode.Forbidden; 
+                return;
+            }
+
+            await _next(httpContext);
+        }
+    }
+}

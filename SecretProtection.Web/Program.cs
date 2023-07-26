689587253224cf353eb9f7b8628582d69a598c3d
Taylan ALTUN <taylanaltun@gmail.com>
2023-07-26 07:56:03 +0300

    Add: SecretProtection project

---
+++ SecretProtection.Web/Program.cs
@@ -0,0 +1,37 @@
+using System.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("SqlConnectionString");
+
+var sqlBuilder = new SqlConnectionStringBuilder(connectionString);
+sqlBuilder.Password = builder.Configuration["Passwords:SqlPassword"];
+
+string conString=sqlBuilder.ToString();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
